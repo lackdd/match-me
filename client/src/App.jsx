4a9d86c21ce9a5d83cf34c86942107207865f835
Andre Berezin <andreberezin@outlook.com>
2025-01-26 20:19:25 +0200

    added nav bar to every page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
-import './App.css'
+import './App.scss'
 import {useEffect, useState} from "react";
+import Navigator from './components/nav-bar/nav-bar-main.jsx';
+import "@fontsource/poppins"; // Defaults to weight 400
+import "@fontsource/poppins/400.css"; // Specify weight
+import "@fontsource/poppins/400-italic.css"; // Specify weight and style
+import {Outlet} from 'react-router-dom';
 
 function App() {
     const [users, setUsers] = useState([]);
@@ -15,22 +20,25 @@ function App() {
 
 
     return (
-        <>
-            <div>
-                <h1>
-                    match me
-                </h1>
-                {users.map(user =>
-                    <div key={user.id}>
-                        <br/>
-                        Name: {user.username}
-                        <br/>
-                        Email: {user.email}
-                    </div>
-                )}
-            </div>
-        </>
-    )
+        <div>
+            {/* Navigator renders on every route */}
+            {/* todo make dynamic so when not logged in show one nav bar and when logged in show the other*/}
+            <Navigator />
+            {/* This is where the current route's component will be displayed */}
+            <Outlet />
+        </div>
+    );
+            {/*<div>*/}
+            {/*    {users.map(user =>*/}
+            {/*        <div key={user.id}>*/}
+            {/*            <br/>*/}
+            {/*            Name: {user.username}*/}
+            {/*            <br/>*/}
+            {/*            Email: {user.email}*/}
+            {/*        </div>*/}
+            {/*    )}*/}
+            {/*</div>*/}
+
 }
 
 export default App
