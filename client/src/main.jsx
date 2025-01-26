3a9f08cb9371537f3a994ed48ef514119ad3f073
Andre Berezin <andreberezin@outlook.com>
2025-01-26 20:18:00 +0200

    added routes for not logged in

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,75 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import {createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.scss'
 import App from './App.jsx'
+import HomePage from './components/homepage/homepage.jsx';
+import Features from './components/features/features.jsx';
+import About from './components/about/about.jsx';
+import Support from './components/support/support.jsx';
+import NotFound from './components/not-found/not-found.jsx';
+import SignUp from './components/signup/signup.jsx';
+import Login from './components/login/login.jsx';
+import Navigator from './components/nav-bar/nav-bar-main.jsx'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        errorElement: <NotFound/>,
+        children: [
+            {
+                path: '/',
+                element: <HomePage />, // This will render inside the <Outlet> in App
+            },
+            {
+                path: '/features',
+                element: <Features />,
+            },
+            {
+                path: '/about',
+                element: <About />,
+            },
+            {
+                path: '/support',
+                element: <Support />,
+            },
+            {
+                path: '/get-started',
+                element: <SignUp />,
+            },
+            {
+                path: '/login',
+                element: <Login />,
+            },
+        ]
+    },
+    // {
+    //     path: '/features',
+    //     element: <Features/>,
+    // },
+    // {
+    //     path: '/about',
+    //     element: <About/>,
+    // },
+    // {
+    //     path: '/support',
+    //     element: <Support/>,
+    // },
+    // {
+    //     path: '/get-started',
+    //     element: <SignUp/>,
+    // },
+    // {
+    //     path: '/login',
+    //     element: <Login/>,
+    // }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      {/*<Navigator/>*/}
+    <RouterProvider router={router} />
+      {/*<App />*/}
   </StrictMode>,
 )
