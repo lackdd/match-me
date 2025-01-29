22aeb33871cc7861837c7c005cfe2353ec830a9e
Andre Berezin <andreberezin@outlook.com>
2025-01-29 12:56:16 +0200

    shortened urls

--- client/src/components/homepage/homepage_footer.jsx
+++ client/src/components/homepage/homepage_footer.jsx
@@ -6,28 +6,28 @@ function HomePageFooter() {
 		<footer className={'homepage-footer'}>
 			<Link to='https://www.facebook.com/' target="_blank" rel="noopener noreferrer">
 				<span>
-					<img src='../../../public/facebook_icon.svg' alt='Facebook' className='icon'/>
+					<img src='/facebook_icon.svg' alt='Facebook' className='icon'/>
 				</span>
 			</Link>
 			<Link to='https://www.instagram.com/' target="_blank" rel="noopener noreferrer">
 				<span>
-					<img src='../../../public/instagram_icon.svg' alt='Instragram' className='icon'/>
+					<img src='/instagram_icon.svg' alt='Instragram' className='icon'/>
 				</span>
 			</Link>
 			<Link to='https://www.x.com/' target="_blank" rel="noopener noreferrer">
 				<span>
-					<img src='../../../public/x_icon.svg' alt='X' className='icon'/>
+					<img src='/x_icon.svg' alt='X' className='icon'/>
 				</span>
 			</Link>
 			<Link to='https://www.youtube.com/' target="_blank" rel="noopener noreferrer">
 				<span>
-					<img src='../../../public/youtube_icon.svg' alt='Youtube' className='icon'/>
+					<img src='/youtube_icon.svg' alt='Youtube' className='icon'/>
 				</span>
 
 			</Link>
 			<Link to='https://www.spotify.com/' target="_blank" rel="noopener noreferrer">
 				<span>
-					<img src='../../../public/spotify_icon.svg' alt='Spotify' className='icon'/>
+				<img src='/spotify_icon.svg' alt='Spotify' className='icon'/>
 				</span>
 			</Link>
 		</footer>
