3e9cbe6d7a7115053196f90cb4e0385291490026
Andre Berezin <andreberezin@outlook.com>
2025-01-29 12:55:15 +0200

    added page skeletons

---
+++ client/src/components/forgot-password/forgot-password.jsx
@@ -0,0 +1,15 @@
+import './forgot-password.scss'
+import {Link} from 'react-router-dom';
+
+function ForgotPassword() {
+	return (
+		<div className='forgot-password-container'>
+			<h1 className={'coming-soon'}>Forgot password</h1>
+			<br/>
+			<h1 className={'coming-soon'}>Coming soon!</h1>
+		</div>
+
+	);
+}
+
+export default ForgotPassword;
--- client/src/components/signup/signup.jsx
+++ client/src/components/signup/signup.jsx
@@ -5,7 +5,9 @@ function SignUp() {
 	return (
 		<>
 			<div className='signup-container'>
-				<h1>Get started</h1>
+				<h1 className={'coming-soon'}>Get started</h1>
+				<br/>
+				<h1 className={'coming-soon'}>Coming soon!</h1>
 			</div>
 		</>
 
