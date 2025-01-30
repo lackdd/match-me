785a6f04c4567d87a7dcc9813bd44dd3079728e3
Andre Berezin <andreberezin@outlook.com>
2025-01-30 14:45:36 +0200

    modified icon links

--- client/src/components/homepage/homepage_footer.jsx
+++ client/src/components/homepage/homepage_footer.jsx
@@ -4,28 +4,28 @@ import {Link} from 'react-router-dom'
 function HomePageFooter() {
 	return (
 		<footer className={'homepage-footer'}>
-			<Link to='https://www.facebook.com/' target="_blank" rel="noopener noreferrer">
+			<Link to='https://www.facebook.com/' target="_blank" rel="noopener noreferrer" title={"Facebook"}>
 				<span>
 					<img src='/facebook_icon.svg' alt='Facebook' className='icon'/>
 				</span>
 			</Link>
-			<Link to='https://www.instagram.com/' target="_blank" rel="noopener noreferrer">
+			<Link to='https://www.instagram.com/' target="_blank" rel="noopener noreferrer" title={"Instagram"}>
 				<span>
 					<img src='/instagram_icon.svg' alt='Instragram' className='icon'/>
 				</span>
 			</Link>
-			<Link to='https://www.x.com/' target="_blank" rel="noopener noreferrer">
+			<Link to='https://www.x.com/' target="_blank" rel="noopener noreferrer" title={"x"}>
 				<span>
 					<img src='/x_icon.svg' alt='X' className='icon'/>
 				</span>
 			</Link>
-			<Link to='https://www.youtube.com/' target="_blank" rel="noopener noreferrer">
+			<Link to='https://www.youtube.com/' target="_blank" rel="noopener noreferrer" title={"Youtube"}>
 				<span>
 					<img src='/youtube_icon.svg' alt='Youtube' className='icon'/>
 				</span>
 
 			</Link>
-			<Link to='https://www.spotify.com/' target="_blank" rel="noopener noreferrer">
+			<Link to='https://www.spotify.com/' target="_blank" rel="noopener noreferrer" title={"Spotify"}>
 				<span>
 				<img src='/spotify_icon.svg' alt='Spotify' className='icon'/>
 				</span>
