885b42f9807761d9b1030b6d1497a6978ced3553
Andre Berezin <andreberezin@outlook.com>
2025-01-30 14:47:09 +0200

    started signup flow UI

---
+++ client/src/components/register/register.jsx
@@ -0,0 +1,166 @@
+import './register.scss'
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+function Register() {
+	const [currentStep, useCurrentStep] = useState(1)
+	const [error, setError] = useState('');
+
+	function ChangeStep() {
+
+	}
+
+	function Step1() {
+		const [firstName, setFirstName] = useState("")
+		const [lastName, setLastName] = useState("")
+		const [gender, setGender] = useState("")
+		const [age, setAge] = useState("")
+		const [email, setEmail] = useState("")
+		const [password, setPassword] = useState("")
+
+
+
+		return (
+
+			<form className='step-one'>
+				<div className='form-title'>
+					<h1>Get started!</h1>
+				</div>
+				<div className={'line'}>
+					<label id='first-name-label' className={'short'}>
+						First name*
+						<br/>
+						<input
+							type='text'
+							id='first-name-input'
+							placeholder='Enter your first name'
+							className={`short ${error ? 'error-border' : ''}`}
+							value={firstName}
+							onChange={(e) => setFirstName(e.target.value)}
+							required
+						/>
+					</label>
+					<label id='last-name-label' className={'short'}>
+						Last name*
+						<br/>
+						<input
+							type='text'
+							id='last-name-input'
+							className={`short ${error ? 'error-border' : ''}`}
+							placeholder='Enter your last name'
+							value={lastName}
+							onChange={(e) => setLastName(e.target.value)}
+							required
+						/>
+					</label>
+				</div>
+				<div className={'line'}>
+					<label id='gender-label' className={'short'}>
+						Gender*
+						<br/>
+						<input
+							type='text'
+							id='gender-input'
+							className={`short ${error ? 'error-border' : ''}`}
+							placeholder='Enter your gender'
+							value={age}
+							onChange={(e) => setGender(e.target.value)}
+							required
+						/>
+					</label>
+					<label id='age-label' className={'short'}>
+						Age*
+						<br/>
+						<input
+							type='number'
+							id='age-input'
+							className={`short ${error ? 'error-border' : ''}`}
+							placeholder='Enter your age'
+							value={age}
+							onChange={(e) => setAge(e.target.value)}
+							required
+						/>
+					</label>
+				</div>
+				<div className={'line large'}>
+					<label id='email-label'>
+						Email address*
+						<br/>
+						<input
+							type='email'
+							id='email-input'
+							className={`${error ? 'error-border' : ''}`}
+							placeholder='Enter your email address'
+							value={age}
+							onChange={(e) => setEmail(e.target.value)}
+							required
+						/>
+					</label>
+				</div>
+				<div className={'line large'}>
+					<label id='password-label'>
+						Password*
+						<br/>
+						<input
+							type='password'
+							id='password-input'
+							className={`${error ? 'error-border' : ''}`}
+							placeholder='Enter a password'
+							value={age}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+						/>
+					</label>
+				</div>
+				<label id='tc-label'>
+					<input
+						type='checkbox'
+						name='terms and conditions'
+						id='tc-input'
+						required/>
+					&nbsp;
+					I agree to the terms and conditions*
+				</label>
+				<label>
+					<button
+						className='next wide small'
+						onClick={ChangeStep}>
+						Next
+					</button>
+				</label>
+			</form>
+		);
+	}
+
+
+	return (
+		<>
+			<div className='register-container'>
+				<div className={'exit-container'}>
+					{/* again a tag to force rerender of nav bar*/}
+					<a href={'/'}>
+						<button className={'button exit'}>Exit</button>
+					</a>
+				</div>
+				<div className={'account-creation'}>
+					Account creation {currentStep}/5
+				</div>
+				<div className={'forms-container'}>
+					{currentStep === 1 && <Step1/>}
+					{currentStep === 2 && <Step2/>}
+					{currentStep === 3 && <Step3/>}
+					{currentStep === 4 && <Step4/>}
+					{currentStep === 5 && <Step5/>}
+				</div>
+				<div id='no-account-label'>
+					Already have an account
+					&nbsp;
+					<Link to='/login'>Sign in</Link>
+				</div>
+			</div>
+		</>
+
+	);
+}
+
+export default Register;
