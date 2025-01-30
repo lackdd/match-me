d4c93dd6b7b37dc67d26693fb2e91c0fb534a01f
Andre Berezin <andreberezin@outlook.com>
2025-01-30 14:45:51 +0200

    added something from the tutorial

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,7 +1,30 @@
 import './login.scss'
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import {useState} from 'react';
+import axios from 'axios';
 
 function Login() {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
+	const history = useNavigate();
+
+	const handleLogin = async() => {
+		try {
+			if (!username || !password) {
+				setError('Please enter a username and a password')
+			}
+			const response = await
+			axios.post('localhost:8080/login', {username, password});
+			console.log('Login successful: ', response.data);
+			history('/dashboard')
+		} catch (err) {
+			console.error('Login failed: ', error.response ? error.response.data : error.message);
+			setError('Invalid username or password.')
+		}
+	}
+
+
 	return (
 			<div className='login-container'>
 				<form className='login-form'>
@@ -12,29 +35,53 @@ function Login() {
 					<label>
 						Email address
 						<br/>
-						<input type='email' placeholder='Enter your email' required/>
+						<input
+							type='email'
+							id='email-input'
+							placeholder='Enter your email'
+							className={`${error ? 'error-border' : ''}`}
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							required
+						/>
 					</label>
 					<label id='password-label'>
 						Password
 						<br/>
 						<input
-							type='password' id='password-input' placeholder='Enter your password' required
+							type='password'
+							id='password-input'
+							className={`${error ? 'error-border' : ''}`}
+							placeholder='Enter your password'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
 						/>
 						<Link to='/forgot-password' id='forgot-password'>forgot password?</Link>
+						<p className={`error-message ${error ? 'visible' : 'hidden'}`}>
+							{error}
+						</p>
 					</label>
 					<label id='remember-me-label'>
-						<input type='checkbox' name='remember' id='remember-me-input'/>
+						<input
+							type='checkbox'
+							name='remember'
+							id='remember-me-input'/>
 						&nbsp;
 						Remember for 30 days
 					</label>
 					<label>
-						<button className='login-button'>Login</button>
+						<button
+							className='login-button wide small'
+						onClick={handleLogin}>
+							Login
+						</button>
 					</label>
 				</form>
 				<div id='no-account-label'>
 					Don't have an account?
 					&nbsp;
-					<Link to='/get-started'>Sign up</Link>
+					<Link to='/register'>Sign up</Link>
 				</div>
 			</div>
 	);
