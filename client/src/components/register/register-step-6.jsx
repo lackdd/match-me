9c9144a0fbd361dde62971d0fadf80e34e9f7bdb
Andre Berezin <andreberezin@outlook.com>
2025-02-12 14:24:40 +0200

    registration final step done

--- client/src/components/register/register-step-6.jsx
+++ client/src/components/register/register-step-6.jsx
@@ -1,34 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+import './loadingAnimation.scss'
+import {useEffect, useState} from 'react';
 
-// step 5 of registration
-function Step6({DeductStep, AddStep}) {
+// step 6 of registration
+function Step6() {
+	const navigate = useNavigate();
+	const [text, setText] = useState("Creating account");
+
+	useEffect(() => {
+		const timer1 = setTimeout(() => {
+			setText("Finding matches");
+		}, 2000);
+		const timer2 = setTimeout(() => {
+			setText("Done!");
+		}, 6000);
+		const timer3 = setTimeout(() => {
+			navigate('/dashboard')
+		}, 8000);
+
+
+		return () => {
+			clearInterval(timer1);
+			clearInterval(timer2);
+			clearInterval(timer3);
+		}
+	}, [navigate])
 
 
 
-	return (
-		<form className={"step-six"}
-			  onSubmit={(e) => {AddStep(e);}}
-			  autoComplete={"off"}
-		>
-			<div className='form-title'>
-				<h1>Account created!</h1>
-				<p>cool animation for 3-5 seconds -> give token -> redirect to /dashboard </p>
+		return (
+			<div className={'spinner-container'}>
+				<div className='spinner'>
+					{text}
+				</div>
 			</div>
-			<div className={'buttons-container'}>
-				<button
-					className='previous wide narrow'
-					type={'button'}
-					onClick={DeductStep}>
-					Previous
-				</button>
-				<button
-					className='next wide narrow'
-					type={'submit'}
-				>
-					Next
-				</button>
-			</div>
-		</form>
-	)
+		)
 }
 
 export default Step6;
