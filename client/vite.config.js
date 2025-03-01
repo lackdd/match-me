0e2f429ce46f182bfc97fd4248fbba983b83a8e9
Andre Berezin <andreberezin@outlook.com>
2025-03-01 20:11:43 +0200

    url for docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,13 +7,13 @@ export default defineConfig({
     proxy: {
       // Proxy for API requests
       '/api': {
-        target: 'http://localhost:8080', // Backend server
+        target: process.env.VITE_BACKEND_URL || (process.env.DOCKER_ENV ? 'http://match-me-backend:8080' : 'http://localhost:8080'), // Backend server, http://match-me-backend:8080 for docker
         changeOrigin: true,            // Ensures the Host header matches the target
         rewrite: (path) => path.replace(/^\/api/, ''), // Optionally rewrite paths
       },
       // Proxy for WebSocket connections (Socket.IO)
       '/socket.io': {
-        target: 'http://localhost:8080', // Backend server
+        target: process.env.VITE_BACKEND_URL || (process.env.DOCKER_ENV ? 'http://match-me-backend:8080' : 'http://localhost:8080'), // Backend server, http://match-me-backend:8080 for docker
         ws: true,                       // Enable WebSocket proxying
         changeOrigin: true,
       },
