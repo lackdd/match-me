2c71fe22a9773f7bc4dc23e7dd6760455936dde3
Andre Berezin <andreberezin@outlook.com>
2025-03-05 20:10:05 +0200

    env variables not available

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "SPRING_FRONTEND_URL: $SPRING_FRONTEND_URL"
+echo "VITE_BACKEND_URL: $VITE_BACKEND_URL"
+echo "POSTGRES_URL: $POSTGRES_URL"
+echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
+echo "VITE_GOOGLE_API: $VITE_GOOGLE_API"
+echo "VITE_GOOGLE_API_KEY: $VITE_GOOGLE_API_KEY"
+
 # Start the backend service (in the background)
 java -jar /app/matchme.jar &
 
