81d54077f168a4fea07499bc91de2ca772aae3f6
Andre Berezin <andreberezin@outlook.com>
2025-03-12 22:25:14 +0200

    added mobile nav bars

--- client/src/components/nav-bar-guest/nav-bar-guest.jsx
+++ client/src/components/nav-bar-guest/nav-bar-guest.jsx
@@ -1,47 +1,132 @@
 import './nav-bar-guest.scss'
 import {NavLink} from 'react-router-dom';
 
+// mobile icons
+import { FiLogIn } from "react-icons/fi";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoClose } from "react-icons/io5";
+import {useState} from 'react';
 
+<IoClose />
 function NavigatorGuest() {
-	return (
-		<nav className='nav-container-guest'>
-			<div className='logo-container'>
-				<NavLink to='/'>
-					<img src='logo_black.png' alt='Jammer' className='nav-bar-logo'/>
-				</NavLink>
-			</div>
-			<div className='links-container'>
-				<NavLink to='/features' className={({ isActive }) =>
-					`features ${isActive ? 'current' : ''}`
-				}>
-					FEATURES
-				</NavLink>
-				<NavLink to='/about' className={({ isActive }) =>
-					`about ${isActive ? 'current' : ''}`
-				}>
-					ABOUT
-				</NavLink>
-				<NavLink to='/support' className={({ isActive }) =>
-					`support ${isActive ? 'current' : ''}`
-				}>
-					SUPPORT
-				</NavLink>
-			</div>
-			<div className='buttons-container'>
-				{/* again a tag to force rerender of nav bar*/}
-				<NavLink to='/register'>
-					<button className='button signup' title='Get started'>
-						Get started
-					</button>
-				</NavLink>
-				<NavLink to='/login'>
-					<button className='button login' title='Log in'>
-						Log in
-					</button>
-				</NavLink>
+	const [hamburger, setHamburger] = useState(false);
 
-			</div>
-		</nav>
+	return (
+		<>
+			<nav className='nav-container-guest default'>
+				<div className='logo-container'>
+					<NavLink to='/'>
+						<img src='logo_black.png' alt='Jammer' className='nav-bar-logo'/>
+					</NavLink>
+				</div>
+				<div className='links-container'>
+					<NavLink to='/features' className={({ isActive }) =>
+						`features ${isActive ? 'current' : ''}`
+					}>
+						FEATURES
+					</NavLink>
+					<NavLink to='/about' className={({ isActive }) =>
+						`about ${isActive ? 'current' : ''}`
+					}>
+						ABOUT
+					</NavLink>
+					<NavLink to='/support' className={({ isActive }) =>
+						`support ${isActive ? 'current' : ''}`
+					}>
+						SUPPORT
+					</NavLink>
+				</div>
+				<div className='buttons-container'>
+					{/* again a tag to force rerender of nav bar*/}
+					<NavLink to='/register'>
+						<button className='button signup' title='Get started'>
+							Get started
+						</button>
+					</NavLink>
+					<NavLink to='/login'>
+						<button className='button login' title='Log in'>
+							Log in
+						</button>
+					</NavLink>
+
+				</div>
+			</nav>
+
+			<nav className='nav-container-guest mobile'>
+				<div className='logo-container'>
+					<NavLink to='/'>
+						<img src='icon_black.png' alt='Jammer' className='nav-bar-logo'/>
+					</NavLink>
+				</div>
+				<div className='links-container'>
+
+					<button
+						// className={`hamburger ${hamburger ? "open" : "closed"}`}
+						className="hamburger"
+						title={'Menu'}
+						onClick={() => setHamburger(!hamburger)}
+					>
+						{hamburger ? (
+							<>
+							<div className={`hamburger-links ${hamburger ? "open" : "closed"}`}>
+								<NavLink to='/features' className={({ isActive }) =>
+									`features ${isActive ? 'current' : ''}`
+								}>
+									Features
+								</NavLink>
+								<NavLink to='/about' className={({ isActive }) =>
+									`about ${isActive ? 'current' : ''}`
+								}>
+									About
+								</NavLink>
+								<NavLink to='/support' className={({ isActive }) =>
+									`support ${isActive ? 'current' : ''}`
+								}>
+									Support
+								</NavLink>
+								<NavLink to='/login' className={({ isActive }) =>
+									`support ${isActive ? 'current' : ''}`
+								}>
+									Login
+								</NavLink>
+								<NavLink to='/register' className={({ isActive }) =>
+									`support ${isActive ? 'current' : ''}`
+								}>
+									Register
+								</NavLink>
+							</div>
+							<IoClose />
+							</>
+						) : (
+							<RxHamburgerMenu />
+						)}
+						{/*{hamburger && <IoClose/>}*/}
+					</button>
+				</div>
+					<div className='buttons-container'>
+						<NavLink to='/login'>
+							<button className='mobile-login' title='Log in'>
+								<FiLogIn />
+							</button>
+						</NavLink>
+					</div>
+				{/*<div className='buttons-container'>*/}
+				{/*	/!* again a tag to force rerender of nav bar*!/*/}
+				{/*	/!*<NavLink to='/register'>*!/*/}
+				{/*	/!*	<button className='mobile-signup' title='Get started'>*!/*/}
+				{/*	/!*		G*!/*/}
+				{/*	/!*	</button>*!/*/}
+				{/*	/!*</NavLink>*!/*/}
+				{/*	<NavLink to='/login'>*/}
+				{/*		<button className='mobile-login' title='Log in'>*/}
+				{/*			L*/}
+				{/*		</button>*/}
+				{/*	</NavLink>*/}
+
+				{/*</div>*/}
+			</nav>
+		</>
+
 	);
 }
 
--- client/src/components/nav-bar-user/nav-bar-user.jsx
+++ client/src/components/nav-bar-user/nav-bar-user.jsx
@@ -4,6 +4,12 @@ import {NavLink, useNavigate} from 'react-router-dom'
 import {useContext} from "react";
 import {useAuth} from '../../AuthContext.jsx';
 
+// mobile icons
+import { FiLogOut } from "react-icons/fi";
+import { BsChat } from "react-icons/bs";
+import { FaUserFriends } from "react-icons/fa";
+import { PiMusicNotesPlus } from "react-icons/pi";
+
 
 function NavigatorUser() {
 	// const { setIsUserLoggedIn } = useContext(AuthContext);
@@ -19,38 +25,75 @@ function NavigatorUser() {
 	};
 
 	return (
-		<nav className='nav-container-user'>
-			<div className='profile-container'>
-				<NavLink to='/dashboard' className={({ isActive }) =>
-					`profile-link ${isActive ? 'current' : ''}`
-				}>
-					<img src='default_profile_picture.png' alt='Profile' className='profile-picture'/>
-					John
-				</NavLink>
-			</div>
-			<div className='links-container'>
-				<NavLink to='/connections' className={({ isActive }) =>
-					`connections ${isActive ? 'current' : ''}`
-				}>
-					CONNECTIONS
-				</NavLink>
-				<NavLink to='/chats' className={({ isActive }) =>
-					`chats ${isActive ? 'current' : ''}`
-				}>
-					CHATS
-				</NavLink>
-				<NavLink to='/recommendations' className={({ isActive }) =>
-					`recommendations ${isActive ? 'current' : ''}`
-				}>
-					RECOMMENDATIONS
-				</NavLink>
-			</div>
-			<div className='buttons-container'>
+		<>
+			<nav className='nav-container-user default'>
+				<div className='profile-container'>
+					<NavLink to='/dashboard' className={({ isActive }) =>
+						`profile-link ${isActive ? 'current' : ''}`
+					}>
+						<img src='default_profile_picture.png' alt='Profile' className='profile-picture'/>
+						John
+					</NavLink>
+				</div>
+				<div className='links-container'>
+					<NavLink to='/connections' className={({ isActive }) =>
+						`connections ${isActive ? 'current' : ''}`
+					}>
+						CONNECTIONS
+					</NavLink>
+					<NavLink to='/chats' className={({ isActive }) =>
+						`chats ${isActive ? 'current' : ''}`
+					}>
+						CHATS
+					</NavLink>
+					<NavLink to='/recommendations' className={({ isActive }) =>
+						`recommendations ${isActive ? 'current' : ''}`
+					}>
+						RECOMMENDATIONS
+					</NavLink>
+				</div>
+				<div className='buttons-container'>
 					<button className='button logout' title='Log out' onClick={handleLogout}>
 						Log out
 					</button>
-			</div>
-		</nav>
+				</div>
+			</nav>
+
+			{/* todo*/}
+			<nav className='nav-container-user mobile'>
+
+				<div className='profile-container'>
+					<NavLink to='/dashboard' className={({ isActive }) =>
+						`profile-link ${isActive ? 'current' : ''}`
+					}>
+						<img src='default_profile_picture.png' alt='Profile' className='profile-picture'/>
+					</NavLink>
+				</div>
+				<div className='links-container'>
+					<NavLink to='/connections' className={({ isActive }) =>
+						`icon connections ${isActive ? 'current' : ''}`
+					}>
+						<FaUserFriends />
+					</NavLink>
+					<NavLink to='/chats' className={({ isActive }) =>
+						`icon chats ${isActive ? 'current' : ''}`
+					}>
+						<BsChat />
+					</NavLink>
+					<NavLink to='/recommendations' className={({ isActive }) =>
+						`icon recommendations ${isActive ? 'current' : ''}`
+					}>
+						<PiMusicNotesPlus />
+					</NavLink>
+				</div>
+				<div className='buttons-container'>
+					<button className='logout' title='Log out' onClick={handleLogout}>
+						<FiLogOut />
+					</button>
+				</div>
+			</nav>
+		</>
+
 	);
 }
 
