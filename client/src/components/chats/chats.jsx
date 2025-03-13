99ed1f70eb71e4200113d6a1f1d716fb9de9419a
Andre Berezin <andreberezin@outlook.com>
2025-03-13 19:51:52 +0200

    chat skeleton

--- client/src/components/chats/chats.jsx
+++ client/src/components/chats/chats.jsx
@@ -4,9 +4,18 @@ import {Link} from 'react-router-dom';
 function Chats() {
 	return (
 		<div className='chats-container'>
-			<h1 className={'coming-soon'}>Chats</h1>
-			<br/>
-			<h1 className={'coming-soon'}>Coming soon!</h1>
+
+			<div className='extra-chats-container'>
+
+				<div className='connections'>
+					connections here
+				</div>
+
+				<div className='chat'>
+					chat here
+				</div>
+
+			</div>
 		</div>
 
 	);
