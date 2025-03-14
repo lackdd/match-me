399b1db86780ba12d4241b754dbae3fd3910f897
Andre Berezin <andreberezin@outlook.com>
2025-03-14 19:35:04 +0200

    finished /connections

--- client/src/components/connections/connections.jsx
+++ client/src/components/connections/connections.jsx
@@ -1,55 +1,244 @@
 import './connections.scss'
-import {Link} from 'react-router-dom';
+
+// react icons
+import { FaRegCircleStop } from "react-icons/fa6";
+import { BsChat } from "react-icons/bs";
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import axios from 'axios';
+
 
 function Connections() {
+	const [loading, setLoading] = useState(true);
+	const [currentConnectionIds, setCurrentConnectionIds] = useState([]);
+	const [currentConnections, setCurrentConnections] = useState([]);
+	const [pendingConnectionIds, setPendingConnectionIds] = useState([]);
+	const [pendingConnections, setPendingConnections] = useState([]);
+	const [isDeleting, setIsDeleting] = useState(false);
+	const [pendingOrCurrent, setPendingOrCurrent] = useState("current");
+	const [currentDataFetched, setCurrentDataFetched] = useState(false);
+	const [pendingDataFetched, setPendingDataFetched] = useState(false);
+
+	const token = useRef(sessionStorage.getItem("token"));
+	console.log("Token: ", token.current)
+
+	const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+
+	const toggleButton = useCallback((event) => {
+		// add .active class to the button and remove .active class from the other button
+		const buttons = document.querySelectorAll('.current-pending-buttons-container button');
+		buttons.forEach(button => {
+			if (button === event.target) {
+				button.classList.add('active');
+			} else {
+				button.classList.remove('active');
+			}
+		});
+	}, []);
+
+
+	const getProfile = async() => {
+		const controller = new AbortController();
+		const signal = controller.signal;
+
+		// make a new route to access another users profile
+
+		// fetch /users/{id} and /users/{id}/profile for the data and then display it
+		// try {
+		// 	const response = await axios.get(`${VITE_BACKEND_URL}/api/`)
+		// }
+
+	}
+	// delete connection
+	const deleteConnection = async(connectionId, setConnections, setConnectionsIds) => {
+
+		try {
+			setIsDeleting(true);
+
+			// todo need api for this
+			await axios.delete(`${VITE_BACKEND_URL}/api/connection/delete/${connectionId}`), {
+				headers: { "Authorization": `Bearer ${token.current}`,
+					"Content-Type": "application/json"},
+			}
+			setConnections(prev => prev.filter(conn => conn._id !== connectionId));
+			setConnectionsIds(prevIds => prevIds.filter(id => id !== connectionId))
+		} catch (error) {
+			if (error.response) {
+				console.error("Backend error:", error.response.data); // Server responded with an error
+			} else {
+				console.error("Request failed:", error.message); // Network error or request issue
+			}
+		} finally {
+			setIsDeleting(false);
+		}
+	}
+
+
+	// fetch connection ids
+	const getConnectionIds = async() => {
+		const controller = new AbortController(); // Create an abort controller
+		const signal = controller.signal;
+
+		try {
+			const [currentConnectionsResponse, pendingConnectionsResponse] = await Promise.all([
+				axios.get(`${VITE_BACKEND_URL}/api/connections`, {
+					headers: { "Authorization": `Bearer ${token.current}`,
+						"Content-Type": "application/json"},
+					signal,
+				}),
+				axios.get(`${VITE_BACKEND_URL}/api/pendingRequests`, {
+					headers: { "Authorization": `Bearer ${token.current}`,
+						"Content-Type": "application/json"},
+					signal,
+				}),
+			])
+
+			const currentIds = currentConnectionsResponse.data;
+			const pendingIds = pendingConnectionsResponse.data;
+
+			await getConnectionsData(currentIds, setCurrentConnections, setCurrentDataFetched);
+			await getConnectionsData(pendingIds, setPendingConnections, setPendingDataFetched);
+
+		} catch (error) {
+			if (axios.isCancel(error)) {
+				console.log("Fetch aborted");
+			} else {
+				console.error("Error getting connections: ", (error.message))
+				// todo display error to user
+			}
+		}
+	}
+
+	// fetch data based on ids
+	const getConnectionsData = async(ids, setState, setFetchedState) => {
+		const controller = new AbortController(); // Create an abort controller
+		const signal = controller.signal;
+
+		try {
+			const requests = ids.map(id =>
+				axios.get(`${VITE_BACKEND_URL}/api/users/${id}`, {
+					headers: { "Authorization": `Bearer ${token.current}`,
+						"Content-Type": "application/json"},
+					signal,
+				})
+			);
+			const responses = await Promise.all(requests);
+			const users = responses.map(response => response.data); // Extract user data
+
+			setState(users);
+			setFetchedState(true); // Indicate that data has been fetched
+
+		} catch (error) {
+			if (axios.isCancel(error)) {
+				console.log("Fetch aborted");
+			} else {
+				if (error.response) {
+					console.error("Backend error:", error.response.data); // Server responded with an error
+				} else {
+					console.error("Request failed:", error.message); // Network error or request issue
+				}
+				// todo display error to user
+			}
+		}
+	}
+
+	// display current connections
+	const displayConnections = (connections) => {
+
+		console.log("length: ", connections.length);
+
+		if (connections.length === 0) {
+
+			if (connections === currentConnections && pendingOrCurrent === "current") {
+				return (
+					<div className='no-connections-message'>No connections yet</div>
+				)
+			}
+
+			if (connections === pendingConnections && pendingOrCurrent === "pending") {
+				return (
+					<div className='no-connections-message'>No requests yet</div>
+				)
+			}
+
+		}
+
+		return connections.map((connection, index) => (
+			<div key={index} className='connection'>
+
+				<div className='picture-name-container'>
+					<img src='profile_pic_female.jpg' alt=''/>
+					{/*<img src={connection.profilePicture} alt={connection.username} className='connection-pic' />*/}
+					<div className='name'>{connection.username}</div>
+				</div>
+
+				<div className='connections-buttons-container'>
+					<button className='no'><FaRegCircleStop /></button>
+					<button className='chat'><BsChat /></button>
+				</div>
+			</div>
+		))
+	}
+
+	// fetch current and pending connection ids
+	useEffect(() => {
+		getConnectionIds();
+	}, []);
+
+	// change loading state when all data is fetched
+	useEffect(() => {
+		if (currentDataFetched && pendingDataFetched) {
+			setLoading(false);
+		}
+	}, [currentDataFetched, pendingDataFetched]);
+
+
+	// log current connections
+	useEffect(() => {
+		console.log("Current connections: ", currentConnections);
+		console.log("Pending connections: ", pendingConnections);
+	}, [currentConnections]);
+
+
 	return (
 		<div className='connections-container'>
 
-			<div className='extra-connections-container'>
-
-				<div className='buttons-container'>
-
-					<button className='current'>Current connections</button>
-
-					<button className='pending'>Pending connections</button>
-
+			{loading && (
+				<div className={'spinner-container'}>
+					<div className='spinner endless'>Finding friends...</div>
 				</div>
+			)}
 
-				<div className='connections-list'>
+			{!loading && (
+				<div className='extra-connections-container'>
 
-					<div className='connection'>
-						<div className='picture-container'>
-							<img src='' alt=''/>
-						</div>
-						<div className='name'>
-							John Smith
-						</div>
-						<div className='connections-buttons-container'>
-							<button className='yes'>yes</button>
-							<button className='no'>no</button>
-						</div>
+					<div className='current-pending-buttons-container'>
+
+						<button className='current active' onClick={() => {
+							toggleButton(event);
+							setPendingOrCurrent('current');
+						}}>Current connections</button>
+
+						<button className='pending' onClick={() => {
+							toggleButton(event);
+							setPendingOrCurrent('pending');
+						}}>Pending connections</button>
+
 					</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
-					<div className='connection'>John Smith</div>
 
+
+					<div className='connections-list'>
+
+						{pendingOrCurrent === 'current' && displayConnections(currentConnections)}
+
+						{pendingOrCurrent === 'pending' && displayConnections(pendingConnections)}
+
+					</div>
 				</div>
-			</div>
-
+			)}
 		</div>
-
 	);
 }
 
 export default Connections;
+
