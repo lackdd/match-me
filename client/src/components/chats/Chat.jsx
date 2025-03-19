41a38bcead3c448fc2e4a07bca4c06b8b5070303
Andre Berezin <andreberezin@outlook.com>
2025-03-19 13:53:33 +0200

    impoved design, added mobile designs, added ability to focus on input with tab key and send message with enter key, added loading state

--- client/src/components/chats/Chat.jsx
+++ client/src/components/chats/Chat.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
-import './chat.css'
+import './chats.scss'
 import { Client } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
 /*import {useAuth} from "../../AuthContext.jsx";*/
 import { useAuth } from '../utils/AuthContext.jsx';
 import axios from "axios";
+
+import { IoMdSend } from "react-icons/io";
+import {IoArrowBack} from 'react-icons/io5';
 
 /*var stompClient = null;*/
 
@@ -106,6 +109,7 @@ const Chat = ({receiverUsername, receiverUserId}) => {
     }, [username]);
 
     const sendMessage = () => {
+        console.log("user id:", userId);
         if (client && message.trim() && receiverUsername) {
             client.publish({
                 destination: "/app/private-message",
@@ -119,27 +123,71 @@ const Chat = ({receiverUsername, receiverUserId}) => {
         }
     };
 
-    /*style={{ border: "1px solid black", padding: "10px", height: "200px", overflowY: "auto" }}*/
-   /* <strong>{msg.senderUsername}: </strong>*/
+    const handleKeyPresses = (event) => {
+        const inputMessage = document.getElementById('message-input');
 
+        if (event.key === "Tab" && inputMessage) {
+
+            if (document.activeElement !== inputMessage) {
+                inputMessage.focus();
+            }
+        }
+
+        const sendButton = document.getElementById('send-message');
+
+        // send message with "Enter" key
+        if (event.key === "Enter" && sendButton) {
+            sendButton.classList.add("active");
+
+            setTimeout(() => {
+                sendButton.classList.remove("active");
+            }, 100);
+
+            sendMessage();
+        }
+    }
+
+    const backToConnections = () => {
+        // todo navigate to connections page
+        const connections = document.getElementById('connections')
+        const chat = document.getElementById('chat')
+
+        if (!chat || !connections) {
+            console.log("chat or connections on found");
+            return;
+        }
+
+        connections.classList.add('show-connections');
+        connections.classList.remove('hide-connections');
+        chat.classList.remove('show-chat');
+        chat.classList.add('hide-chat');
+    }
+
+    // todo add "typing..." indicator when other user is typing
     return (
-        <div className="chat-box">
-            <h2>{receiverUsername}</h2>
-            <div className="chat">
+        <div className="chat-box" onKeyDown={handleKeyPresses}>
+            <div className='heading-container'>
+                <button className='toggle-connections' onClick={backToConnections} tabIndex={-1}><IoArrowBack /></button>
+                <h2>{receiverUsername}</h2>
+            </div>
+            <div className="chat-messages">
                 {messages.map((msg, index) => (
                     <div key={index} className={`message-box ${msg.senderUsername === username ? "sent" : "received"}`}>
                         {msg.content}
                     </div>
                 ))}
             </div>
-            <input
-                className="input"
-                type="text"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                placeholder="Type a message..."
-            />
-            <button onClick={sendMessage}>Send</button>
+            <div className='input-container'>
+                <input
+                    id={'message-input'}
+                    className="input"
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    placeholder="Type a message..."
+                />
+                <button id={'send-message'} onClick={sendMessage} tabIndex={-1}><IoMdSend /></button>
+            </div>
         </div>
     );
 };
--- client/src/components/chats/chats.jsx
+++ client/src/components/chats/chats.jsx
@@ -3,10 +3,15 @@ import {Link} from 'react-router-dom';
 import Chat from "../chats/Chat";
 /*import {useAuth} from '../../AuthContext.jsx';*/
 import { useAuth } from '../utils/AuthContext.jsx';
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 
+// react icons
+import { IoArrowBack } from "react-icons/io5";
+import { IoArrowForward } from "react-icons/io5";
+
 function Chats() {
+	const [loading, setLoading] = useState(true);
 	const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 	const { tokenValue } = useAuth();
 	const [connections, setConnections] = useState([]);
@@ -23,6 +28,7 @@ function Chats() {
 				console.log("Setting connections:", response.data);
 				setConnections(response.data);
 
+				// todo get profile pictures as well
 				if (response.data.length > 0) {
 					const userDetailsResponse = await axios.post(`${VITE_BACKEND_URL}/api/getUsersByIds`,
 						response.data,
@@ -34,6 +40,8 @@ function Chats() {
 				}
 			} catch (error) {
 				console.log(error.message);
+			} finally {
+				setLoading(false);
 			}
 
 		}
@@ -41,37 +49,70 @@ function Chats() {
 	}, []);
 
 	const handleButtonClick = (userId, username) => {
+		const inputMessage = document.getElementById('message-input');
+
+		if (inputMessage) {
+			inputMessage.focus();
+		}
+
 		setSelectedUser(username);
 		setSelectedUserId(userId);
 	};
 
+	const openChat = () => {
+		// open chat
+		const connections = document.getElementById('connections')
+		const chat = document.getElementById('chat')
+
+		if (!chat || !connections) {
+			console.log("chat or connections on found");
+			return;
+		}
+
+		connections.classList.remove('show-connections');
+		connections.classList.add('hide-connections');
+		chat.classList.remove('hide-chat');
+		chat.classList.add('show-chat');
+	}
+
+	// todo add online/offline indicators
 	return (
 		<div className='chats-container'>
 
-			<div className='extra-chats-container'>
-
-				<div className='connections'>
-					<h3>Connections</h3>
-					{connectionDetails.length === 0 ? (
-						<p>No connections</p>
-					) : (
-						connectionDetails.map((connection) => (
-					<button key={connection.id} onClick={() => handleButtonClick(connection.id, connection.username)}>
-						{connection.username}
-					</button>
-						))
-					)}
+			{loading && (
+				<div className={'spinner-container'}>
+					<div className='spinner endless'>Loading chats...</div>
 				</div>
+			)}
 
-				<div className='chat'>
-					{selectedUser ? (
-						<Chat receiverUsername={selectedUser} receiverUserId={selectedUserId} />
-					) : (
-						<p>Select a user to start chatting</p>
-					)}
-				</div>
+				{!loading && (
+					<div className='extra-chats-container'>
+					<div className='connections' id={'connections'}>
+						{connectionDetails.length === 0 ? (
+							<p>No connections</p>
+						) : (
+							connectionDetails.map((connection) => (
+								<button className={`picture-name-button ${selectedUserId === connection.id ? 'selected' : ''}`} key={connection.id} onClick={() => {
+									handleButtonClick(connection.id, connection.username);
+									openChat();
+								}}>
+									<img src='profile_pic_female.jpg' alt='' className='profile-picture'/>
+									<div className='name'>{connection.username}</div>
+								</button>
+							))
+						)}
+					</div>
 
+					<div className='chat' id={'chat'}>
+				{selectedUser ? (
+					<Chat receiverUsername={selectedUser} receiverUserId={selectedUserId} />
+				) : (
+					<p className={'no-chat'}>Select a user to start chatting</p>
+				)}
 			</div>
+					</div>
+				)}
+
 		</div>
 
 	);
