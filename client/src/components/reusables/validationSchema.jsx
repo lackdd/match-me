73d18df031b529ef9c2773c1ff86778fbf67ca73
Andre Berezin <andreberezin@outlook.com>
2025-03-20 16:34:17 +0200

    added temporary duplicate schemas

--- client/src/components/reusables/validationSchema.jsx
+++ client/src/components/reusables/validationSchema.jsx
@@ -261,3 +261,33 @@ export const stepTwoSchemaDashboard =
 				)
 				.min(1, "Required"),
 	});
+
+export const stepThreeSchemaDashboard =
+	yup.object().shape({
+		experience: yup
+			.number()
+			.required("Required")
+			.typeError("Required")
+			.min(0, "Minimum 0 years")
+			// .max(
+			// 	formOneData.age, // Use formOneData.age directly here
+			// 	`Maximum your age: ${formOneData.age}`
+			// ), // Dynamically use formOneData.age
+			.max(120),
+		location: yup.object().shape({
+				label: yup.string().required(),
+				value: yup.string().required(),
+			}
+		)
+			.typeError("Required")
+			.required("Required"), // Ensures it's not empty
+		musicLink: yup
+			.string()
+			.url("Must be a link")
+			.notRequired(),
+		description: yup
+			.string()
+			.min(0, "")
+			.max(300, "Maximum 300 characters")
+			.notRequired(),
+	});
