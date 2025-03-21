89d18e7787757046cd2742c1f8f50ec066fcbf67
Andre Berezin <andreberezin@outlook.com>
2025-03-21 22:16:13 +0200

    added features page content

--- client/src/components/features/features.jsx
+++ client/src/components/features/features.jsx
@@ -1,13 +1,39 @@
 import './features.scss'
+import { SiMusicbrainz } from "react-icons/si";
+import { PiMusicNotesPlus } from "react-icons/pi";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
 
 
 function Features() {
 	return (
 		<>
 			<div className='features-container'>
-				<h1 className={'coming-soon'}>Features</h1>
-				<br />
-				<h1 className={'coming-soon'}>Coming soon!</h1>
+
+				<div className='features-list'>
+
+					<div className='feature default' id={'matches'}>
+						<SiMusicbrainz />
+						<p className='text'>
+							Our algorithm matches you with other musicians based on your preferences such as age, gender, location, years of experience, the types of genres you prefer, the way you prefer to make music and what goals you have.
+						</p>
+					</div>
+
+					<div className='feature reverse' id={'connections'}>
+						<PiMusicNotesPlus />
+						<p className='text'>
+							Choose which of the suggested matches you'd like to connect with in a tinder-like manner. See the list of your current and pending connections. Accept pending connections to make more friends or decline them.
+						</p>
+					</div>
+
+
+					<div className='feature default' id={'more-soon'}>
+						<IoChatboxEllipsesOutline />
+						<p className='text'>
+							Chat securely with your connect to share passion and ideas. Arrange jam sessions and get to know your connections.
+						</p>
+					</div>
+
+				</div>
 			</div>
 		</>
 
