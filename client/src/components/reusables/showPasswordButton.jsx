4cb73252672d03c42931e296ddd12ca6819e8949
Andre Berezin <andreberezin@outlook.com>
2025-03-26 10:39:06 +0200

    added if block for change password form

--- client/src/components/reusables/showPasswordButton.jsx
+++ client/src/components/reusables/showPasswordButton.jsx
@@ -2,7 +2,7 @@ import {FiEye, FiEyeOff} from 'react-icons/fi';
 import './showPasswordButton.scss'
 
 
-export function ShowPasswordButton({showPassword, setShowPassword, login = false}) {
+export function ShowPasswordButton({showPassword, setShowPassword, login = false, register = false, changePassword = false }) {
 	return (
 		<button
 			tabIndex={-1}
@@ -13,9 +13,13 @@ export function ShowPasswordButton({showPassword, setShowPassword, login = false
 				() => {
 					if (login) {
 						document.getElementById('password-login').type = document.getElementById('password-login').type === 'password' ? 'text' : 'password';
-					} else {
+					} else if (register) {
 						document.getElementById('password').type = document.getElementById('password').type === 'password' ? 'text' : 'password';
 						document.getElementById('rePassword').type = document.getElementById('rePassword').type === 'password' ? 'text' : 'password';
+					} else {
+						document.getElementById('currentPassword').type = document.getElementById('currentPassword').type === 'password' ? 'text' : 'password';
+						document.getElementById('newPassword').type = document.getElementById('newPassword').type === 'password' ? 'text' : 'password';
+						document.getElementById('reNewPassword').type = document.getElementById('reNewPassword').type === 'password' ? 'text' : 'password';
 					}
 					setShowPassword(!showPassword);
 				}
