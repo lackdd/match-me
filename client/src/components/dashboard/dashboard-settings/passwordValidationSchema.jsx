8da5b4c774982988d47e35d8094f15b4314afc09
Andre Berezin <andreberezin@outlook.com>
2025-03-30 11:46:46 +0300

    added max character limits to form fields for security

--- client/src/components/dashboard/dashboard-settings/passwordValidationSchema.jsx
+++ client/src/components/dashboard/dashboard-settings/passwordValidationSchema.jsx
@@ -3,12 +3,15 @@ import * as yup from 'yup';
 export const passwordValidationSchema =
 	yup.object().shape({
 		currentPassword: yup.string()
-			.required("Required"),
+			.required("Required")
+			.max(30, "Max 30 characters"),
 		newPassword: yup.string()
 			.required("Required")
 			.min(3, "Must contain at least 3 characters")
+			.max(30, "Max 30 characters")
 			.matches(/[0-9]/, "Must contain at least one number"),
 		reNewPassword: yup.string()
 			.oneOf([yup.ref("newPassword"), null], "Passwords must match")
-			.required("Required"),
+			.required("Required")
+			.max(30, "Max 30 characters"),
 	});
