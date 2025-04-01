991b93678ecd8ab5c365420d8a19d92830db6e70
Andre Berezin <andreberezin@outlook.com>
2025-04-01 20:57:53 +0300

    removed old code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,49 +35,6 @@ function App() {
         loadGoogleMapsScript();
     }, []);
 
-
-    // useEffect(() => {
-    //     const validateToken = async () => {
-    //         const token = sessionStorage.getItem("token");
-    //         if (token){
-    //             try {
-    //                 const response = await
-    //                     axios.post(`${VITE_BACKEND_URL}/api/validateToken`, {},
-    //                         {
-    //                             headers: { Authorization: `Bearer ${token}` },
-    //                         }
-    //                     );
-    //                 console.log("Token is valid. Logging in")
-    //                 setIsUserLoggedIn(true);
-    //             } catch (error) {
-    //                 "Failed to validate token. Token either missing or not correct."
-    //                 setIsUserLoggedIn(false);
-    //             }
-    //         }
-    //     };
-    //
-    //     validateToken();
-    // }, []);
-    // useEffect(() => {
-    //     const token = sessionStorage.getItem("token");
-    //
-    //     if (token) {
-    //         setIsUserLoggedIn(true);
-    //     }
-    // }, [sessionStorage.getItem("token")]);
-
-    // const [isUserLoggedIn, setIsUserLoggedIn] = useState(() => {
-    //     return sessionStorage.getItem("token") ? true : false;
-    // });
-
-
-    // Define routes where the guest NavBar should be shown
-    const guestRoutes = ["/", "/login", "/forgot-password", "/features", "/about", "/support", "/forgot-password"];
-    const userRoutes = ["/dashboard", "/chats", "/recommendations", "/connections", "/settings"];
-
-    // Check if the current route is in guestRoutes
-    const isGuestRoute = guestRoutes.includes(location.pathname);
-    const isUserRoute = userRoutes.includes(location.pathname);
     const isRegister = location.pathname === "/register";
 
     if (isLoading) {
@@ -85,37 +42,13 @@ function App() {
     }
 
     return (
-        // <AuthContext.Provider value={{isUserLoggedIn, setIsUserLoggedIn}}>
             <div>
-                {/*{!isRegister ?*/}
-                {/*    (isUserLoggedIn === false ? <NavigatorGuest /> :*/}
-                {/*        isUserLoggedIn === true ? <NavigatorUser /> : null) : null}*/}
                 {!isRegister ? (
                     isUserLoggedIn ? <NavigatorUser /> : <NavigatorGuest />
                 ) : null}
-
-                {/*{!isUserLoggedIn && !isRegister && <NavigatorGuest/>}*/}
-                {/*{isUserLoggedIn && !isRegister &&  <NavigatorUser/>}*/}
-
-                {/*{isGuestRoute && !isRegister && <NavigatorGuest/>}*/}
-                {/*{isUserRoute && isUserLoggedIn && !isRegister &&  <NavigatorUser/>}*/}
                 <Outlet/>
             </div>
-
-
-
     );
-            {/*<div>*/}
-            {/*    {users.map(user =>*/}
-            {/*        <div key={user.id}>*/}
-            {/*            <br/>*/}
-            {/*            Name: {user.username}*/}
-            {/*            <br/>*/}
-            {/*            Email: {user.email}*/}
-            {/*        </div>*/}
-            {/*    )}*/}
-            {/*</div>*/}
-
 }
 
 export default App
